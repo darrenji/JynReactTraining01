e33ef60ee3bf072eab224a1cf0d8ba885b5da7f5
darren <764190362@qq.com>
2016-07-11 11:50:17 +0800

    finish01

---
+++ app/index.js
@@ -0,0 +1,57 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var USER_DATA = {
+    name: 'darrenji',
+    username: 'darren',
+    image: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+}
+
+var ProfilePic = React.createClass({
+    render: function(){
+        return <img src={this.props.imageUrl} style={{height:100, width: 100}} />
+    }
+});
+
+var ProfileLink = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <a href={'https://www.github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
+        )
+    }
+});
+
+
+var ProfileName = React.createClass({
+    render: function(){
+        return (
+            <div>
+                {this.props.name}
+            </div>
+        )
+    }
+})
+
+var Avatar = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
+        )
+    }
+})
+
+
+
+ReactDOM.render(
+    <Avatar name="darren" user={USER_DATA} />,
+    document.getElementById('app')
+)
