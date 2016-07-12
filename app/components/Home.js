260de4e0ad8200f62c0a344e99584a3946700dd8
darren <764190362@qq.com>
2016-07-12 16:46:42 +0800

    finish18

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,17 +2,18 @@ var React = require('react');
 var transparentBg = require('../styles').transparentBg;
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
+var MainContainer = require('./MainContainer');
 
 var Home = React.createClass({
     render: function(){
         return (
-            <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+            <MainContainer>
                 <h2>Github擂台赛</h2>
                 <p>无论你来自哪里，我们在这里决一胜负</p>
                 <Link to='/playerOne'>
                     <button type='button' className='btn btn-lg btn-success'>走起</button>
                 </Link>
-            </div>
+             </MainContainer>
         )
     }
 });
