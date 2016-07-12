bafee4e08bc13f4c3aa45eb9e1d2ae7cb2003973
darren <764190362@qq.com>
2016-07-12 15:22:41 +0800

    finish14

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -6,14 +6,25 @@ var ConfirmBattleContainer = React.createClass({
      router: React.PropTypes.object.isRequired  
    },
    getInitialState: function(){
+     console.log('getIntialState');
      return {
          isLoading: true,
          playerInfo:[]
      }
    },
+   componentWillMount: function(){
+     console.log('componentWillMount');
+   },
    componentDidMount: function(){
        var query = this.props.location.query;
        //根据路由查询字符串参数到github获取数据，并更新状态
+       console.log('componentDidMount');
+   },
+   componentWillReceiveProps: function(){
+       console.log('componentWillReceiveProps')  ;
+   },
+   componentWillUnmount: function(){
+       console.log('componentWillUnmount');
    },
    render: function(){
        return (
